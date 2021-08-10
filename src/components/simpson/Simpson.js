3f4da36858a849871e17e665975112503cb94452
bigbird <harachikzarema@gmail.com>
2021-08-10 21:21:17 +0300

    init

---
+++ src/components/simpson/Simpson.js
@@ -0,0 +1,13 @@
+export default function Simpson({name,surname,age,info,photo}){
+
+    return(
+<div className={"simpson"}>
+    <h2>{name} {surname}, age is {age} years</h2>
+
+    {/*<h4>{age}</h4>*/}
+    <p>{info}</p>
+    <img src={photo} alt=""/>
+    <hr/>
+</div>
+    );
+}
